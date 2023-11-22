816a62a17d0fc719e3ef44d99dbd1eb48b26c850
arjunGOAT <arjunyermalkumar@gmail.com>
2023-11-21 22:19:00 -0500

    Login Register Progress

--- Jum project/Assets/Scenes/Register.cs
+++ Jum project/Assets/Scenes/Register.cs
@@ -1,13 +1,26 @@
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.io;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Register : MonoBehaviour
 {
+    public Inputfield username;
+    public Inputfield password;
+    public Button registerButton;
+    public Button goTologInButton;
+
+    Arraylist credentials;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        credentials = new Arraylist(File.ReadAllLines(Application.dataPath + "/credentials.txt"));
     }
 
     // Update is called once per frame
