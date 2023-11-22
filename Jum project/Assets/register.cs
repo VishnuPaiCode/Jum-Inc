fd5b21e58a30d5f40974e9b00c7a1d163a67e773
VishnuPaiCode <nitinpai1010@gmail.com>
2023-11-22 14:19:59 -0500

    rgloquyf34gler.jf

--- Jum project/Assets/register.cs
+++ Jum project/Assets/register.cs
@@ -1,22 +1,13 @@
-
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
+public class register : MonoBehaviour
 {
-    
-
-    
-
-
     // Start is called before the first frame update
     void Start()
     {
-    
+        
     }
 
     // Update is called once per frame
